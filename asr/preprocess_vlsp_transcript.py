7ea30fa10810f62ff5cd3ec05305d90537a00b2b
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2020-05-12 00:41:04 +0700

    Update

--- asr/preprocess_vlsp_transcript.py
+++ asr/preprocess_vlsp_transcript.py
@@ -3,47 +3,60 @@ import os
 import json
 import random
 import shutil
+import multiprocessing
 
 parser = argparse.ArgumentParser(description="Preprocess transcript for vivos dataset")
 
 parser.add_argument("transcript", type=str, help="transcript file")
-parser.add_argument("--train_dir", type=str, help="Training set dir")
-parser.add_argument("--dev_dir", type=str, help="Dev set dir")
-parser.add_argument("--test_dir", type=str, help="Testing set dir")
-parser.add_argument("--output", type=str, help="output transcript file")
+parser.add_argument("--output_dir", "-o", type=str, help="output dir")
 
 args = parser.parse_args()
 
 with open(args.transcript, "r", encoding="utf-8") as i:
   lines = i.read().splitlines()
 
-train_set = int(len(lines) * 0.8)
-test_set = len(lines) - train_set
-dev_set = int(len(train_set) * 0.1)
-train_set = train_set - dev_set
+train_set_count = int(len(lines) * 0.8)
+test_set_count = len(lines) - train_set_count
+dev_set_count = int(train_set_count * 0.1)
+train_set_count = train_set_count - dev_set_count
 
 random.shuffle(lines)
-train_set = lines[0:train_set]
-dev_set = lines[train_set+1:train_set+dev_set]
-test_set = lines[train_set+dev_set+1:]
+train_set = lines[0:train_set_count]
+dev_set = lines[train_set_count+1:train_set_count+dev_set_count]
+test_set = lines[train_set_count+dev_set_count+1:]
 
 cur_parent_dir = os.path.dirname(args.transcript)
 tokens = ".,;/\\\"@#$%^&*()_-+={}[]~`-|?!1234567890"
 
-for parent_dir, dataset in [[args.train_dir, train_set], [args.dev_dir, dev_set], [args.test_dir. test_set]]:
+slices = [[train_set, "train"], [dev_set, "dev"], [test_set, "test"]]
+
+def write_transcript(dataset, mode="train"):
+  with open(os.path.join(args.output_dir, f"{mode}_transcripts.tsv"), "w", encoding="utf-8") as o:
+    for audio_path, duration, text in dataset:
+      o.write(f"{audio_path}\t{duration}\t{text}\n")
+
+def map_fn(aslice):
+  dataset, mode = aslice
   for idx, line in enumerate(dataset):
-    line = json.load(line)
+    line = json.loads(line)
     key = line["key"].split("/")
-    key = key[4:]
+    key = key[5:]
     key = "/".join(key)
     cur_key = os.path.join(cur_parent_dir, key)
-    key = os.path.join(parent_dir, key)
+    key = os.path.join(args.output_dir, key)
+    try:
+      os.makedirs(key)
+    except Exception:
+      pass
     shutil.move(cur_key, key)
     if any([token in line["text"] for token in tokens]):
       print(line["text"])
       line["text"] = str(input("Input: "))
-    lines[idx] = [key, line["duration"], line["text"].lower()]
+    dataset[idx] = [key, line["duration"], line["text"].lower()]
+    print(f"\r{key}", end="")
+  write_transcript(dataset, mode=mode)
+  print(f"\nDone processing {mode} dataset")
 
-with open(args.output, "w", encoding="utf-8") as o:
-  for audio_path, duration, text in lines:
-    o.write(f"{audio_path}\t{duration}\t{text}\n")
+
+with multiprocessing.Pool(3) as pool:
+  pool.map(map_fn, slices)
