2b94d352222fc791ab8d3de5e4cee5650625f8e9
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2020-05-22 11:24:20 +0700

    Update

---
+++ asr/get_dataset_part.py
@@ -0,0 +1,41 @@
+import os
+import argparse
+import shutil
+import random
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--transcript", "-t", type=str, help="Transcript file")
+parser.add_argument("--output", "-o", type=str, help="Output dir")
+parser.add_argument("percent", type=float, help="Percent")
+
+args = parser.parse_args()
+
+with open(args.transcript, "r", encoding="utf-8") as i:
+  lines = i.read().splitlines()
+  lines = lines[1:]
+
+
+random.shuffle(lines)
+lines = lines[:int(args.percent * len(lines))]
+
+for line in lines:
+  line = line.split("\t")
+  src = line[0]
+  path = line[0].split("/")
+  path = path[-4:]
+  path = "/".join(path)
+  path = os.path.join(args.output, path)
+  try:
+    os.makedirs(os.path.dirname(path))
+  except Exception:
+    pass
+    shutil.copyfile(src, path)
+  print(f"\rProcessed: {path}", end="")
+
+with open(os.path.join(args.output, os.path.basename(args.transcript)), "w", encoding="utf-8") as o:
+  o.write("PATH\tDURATION\tTRANSCRIPT\n")
+  for line in lines:
+    o.write(f"{line}\n")
+
+print("\nDone processing.")
