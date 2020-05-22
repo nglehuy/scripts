a7155cd40b942c4b6946cbc009e45ccecc03ce06
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2020-05-23 00:19:02 +0700

    Update

--- asr/get_dataset_part.py
+++ asr/get_dataset_part.py
@@ -1,24 +1,26 @@
 import argparse
 import random
+import sys
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--transcript", "-t", type=str, help="Transcript file")
 parser.add_argument("--output", "-o", type=str, help="Output dir")
-parser.add_argument("percent", type=float, help="Percent")
+parser.add_argument("ratio", type=float, help="Percent")
 
 args = parser.parse_args()
 
 with open(args.transcript, "r", encoding="utf-8") as i:
   lines = i.read().splitlines()
+  firstline = lines[0]
   lines = lines[1:]
 
 random.shuffle(lines)
-lines = lines[:int(args.percent * len(lines))]
+lines = lines[:int(args.ratio * len(lines))]
 
 with open(args.output, "w", encoding="utf-8") as o:
-  o.write("PATH\tDURATION\tTRANSCRIPT\n")
-  for line in lines:
+  o.write(f"{firstline}\n")
+  for i, line in enumerate(lines, 1):
     o.write(f"{line}\n")
-
-print("\nDone processing.")
+    sys.stdout.write("\033[K")
+    print(f"\rProcessed {i}/{len(lines)}", end="")
